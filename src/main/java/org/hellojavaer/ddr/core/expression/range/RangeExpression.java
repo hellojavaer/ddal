4446b97f66303e52512a0866fec1dc09948bc31a
zoukaiming <hellojavaer@gmail.com>
2016-12-16 01:38:53 +0800

    optimize range expression

--- src/main/java/org/hellojavaer/ddr/core/expression/range/RangeExpression.java
+++ src/main/java/org/hellojavaer/ddr/core/expression/range/RangeExpression.java
@@ -22,36 +22,13 @@ package org.hellojavaer.ddr.core.expression.range;
 public class RangeExpression {
 
     public static void parse(String str, RangeItemVisitor itemVisitor) {
-        boolean escape = false;
-        int nextStart = 0;
-        int level = 0;// 语义深度
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (escape) {
-                if (ch == '\\' || ch == '[' || ch == ']' || ch == '~' || ch == ',') {
-                    escape = false;
-                } else {
-                    throw new RangeExpressionException(str, i, ch, '\\', '[', ']', '~', ',');
-                }
-            } else {
-                if (ch == '\\') {
-                    escape = true;
-                } else if (ch == ',') {// 优化
-                    if (level == 0) {
-                        parse(str, null, nextStart, i, itemVisitor);
-                        nextStart = i + 1;
-                    }
-                } else if (ch == '[') {
-                    level++;
-                } else if (ch == ']') {
-                    level--;
-                }
-            }
+        int startIndex = 0;
+        while (startIndex < str.length()) {
+            startIndex = parse(str, null, startIndex, itemVisitor);
         }
-        parse(str, null, nextStart, str.length(), itemVisitor);
     }
 
-    private static void parse(String str, String prefix, int startIndex, int endIndex, RangeItemVisitor itemVisitor) {
+    private static int parse(String str, String prefix, int startIndex, RangeItemVisitor itemVisitor) {
         boolean escape = false;
         StringBuilder sb = null;
         if (prefix != null) {
@@ -59,39 +36,42 @@ public class RangeExpression {
             sb.append(prefix);
         }
         for (int i = startIndex;; i++) {
-            if (i > endIndex) {
-                return;
-            } else if (i == endIndex) {
-                if (sb != null) {
-                    itemVisitor.visit(sb.toString());
-                } else {
-                    itemVisitor.visit(str.substring(startIndex, endIndex));
+            if (i >= str.length()) {
+                if (escape) {
+                    throw new RangeExpressionException(str, str.length(), (char) 0,
+                                                       "expect a character('\\', '[', '], ',') to be escaped");
                 }
-                return;
-            }
-            char ch = str.charAt(i);
-            if (ch == ',') {
                 if (sb != null) {
                     itemVisitor.visit(sb.toString());
-                    sb = null;
                 } else {
                     itemVisitor.visit(str.substring(startIndex, i));
                 }
-            } else if (escape) {// 处理转义
-                if (ch == '[' || ch == ']' || ch == '~' || ch == ',' || ch == '\\') {//
+                return i;
+            }
+            char ch = str.charAt(i);
+            if (escape) {// 语句块外特殊字符
+                if (ch == '[' || ch == ']' || ch == ',' || ch == '\\') {//
                     sb.append(ch);
                     escape = false;
                 } else {
-                    throw new RangeExpressionException(str, i, ch, '\\', '[', ']', '~', ',');
+                    throw new RangeExpressionException(str, i, ch,
+                                                       "Out of statement block, escape character '\\' can only be used for character '\\', ',' , '[' and ']' ");
                 }
             } else {//
-                if (ch == '\\') {// 转义后使用sb做缓存,否则直接截取str子串
+                if (ch == ',') {// 递归终结符
+                    if (sb != null) {
+                        itemVisitor.visit(sb.toString());
+                    } else {
+                        itemVisitor.visit(str.substring(startIndex, i));
+                    }
+                    return i;
+                } else if (ch == '\\') {// 转义后使用sb做缓存,否则直接截取str子串
                     if (sb == null) {
                         sb = new StringBuilder();
                     }
                     escape = true;
                 } else if (ch == '[') {// 区间开始符号
-                    String rangPrefix = "";
+                    String rangPrefix = null;
                     if (sb != null) {
                         rangPrefix = sb.toString();
                     } else {
@@ -102,7 +82,7 @@ public class RangeExpression {
                     int startNum = 0;
                     int endNum = 0;
                     for (;; j++) {
-                        if (j >= endIndex) {
+                        if (j >= str.length()) {
                             throw new RangeExpressionException(str, j, (char) 0, "expect closed expression. eg: [0~9]'");
                         }
                         char ch0 = str.charAt(j);
@@ -124,7 +104,7 @@ public class RangeExpression {
                     j++;
                     i = j;
                     for (;; j++) {
-                        if (j >= endIndex) {
+                        if (j >= str.length()) {
                             throw new RangeExpressionException(str, j, (char) 0, ']');
                         }
                         char ch0 = str.charAt(j);
@@ -144,16 +124,17 @@ public class RangeExpression {
                     }
                     int nextStart = ++j;
                     i = j;
+                    int e = 0;
                     if (startNum <= endNum) {// 升区间
                         for (int k = startNum; k <= endNum; k++) {
-                            parse(str, rangPrefix + k, nextStart, endIndex, itemVisitor);
+                            e = parse(str, rangPrefix + k, nextStart, itemVisitor);
                         }
-                        return;
+                        return e;
                     } else {// 降区间
                         for (int k = startNum; k >= endNum; k--) {
-                            parse(str, rangPrefix + k, nextStart, endIndex, itemVisitor);
+                            e = parse(str, rangPrefix + k, nextStart, itemVisitor);
                         }
-                        return;
+                        return e;
                     }
                 } else if (ch == ']') {
                     throw new RangeExpressionException(str, i, ch, "expect closed expression. eg: [0~9]");
@@ -165,7 +146,7 @@ public class RangeExpression {
                     }
                 }
             }
-        }
+        }// for
     }
 
 }
