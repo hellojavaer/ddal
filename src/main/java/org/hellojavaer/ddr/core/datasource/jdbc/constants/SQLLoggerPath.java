49e1268f0be09fe78ab3146f1fdb2a5cb006a87f
Kaiming Zou <hellojavaer@gmail.com>
2016-12-24 22:00:00 +0800

    update

--- src/main/java/org/hellojavaer/ddr/core/datasource/jdbc/constants/SQLLoggerPath.java
+++
@@ -1,9 +0,0 @@
-package org.hellojavaer.ddr.core.datasource.jdbc.constants;
-
-/**
- *
- * @author <a href="mailto:hellojavaer@gmail.com">Kaiming Zou</a>,created on 24/12/2016.
- */
-public class SQLLoggerPath {
-
-}
