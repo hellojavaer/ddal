98633035ef85f8e61a0e43ca00a43bb0775b5135
Kaiming Zou <hellojavaer@gmail.com>
2017-01-14 18:13:14 +0800

    update

--- ddal-ddr/src/main/java/org/hellojavaer/ddal/ddr/shard/ShardRouter.java
+++ ddal-ddr/src/main/java/org/hellojavaer/ddal/ddr/shard/ShardRouter.java
@@ -16,7 +16,16 @@
 package org.hellojavaer.ddal.ddr.shard;
 
 /**
+ * All the shard route ways designed in 'DDR' can be summarized as two ways.
+ * <pre>
+ * The one: one table one shard value.
+ *    this case can't used to to divide data which don't have strongly relevance. the great advantages of this way is you can use aggregate function in a ordinary way.
+ *    But as the limit you may lose some load balancing for reading and writing.
  *
+ * The other: one table multiple shard values.
+ *   this dividing way can implement load balancing for reading and writing. But as the limit you can't use aggregate function in a ordinary way.
+ *
+ * </pre>
  * @author <a href="mailto:hellojavaer@gmail.com">Kaiming Zou</a>,created on 05/11/2016.
  */
 public interface ShardRouter {
--- ddal-ddr/src/main/java/org/hellojavaer/ddal/ddr/shard/annotation/ShardRoute.java
+++ ddal-ddr/src/main/java/org/hellojavaer/ddal/ddr/shard/annotation/ShardRoute.java
@@ -20,7 +20,17 @@ import org.hellojavaer.ddal.ddr.shard.enums.ContextPropagation;
 import java.lang.annotation.*;
 
 /**
+ * Setting the default route information for the schema
+ * <pre>
+ * This configuration is fully equivalent to invoking #{org.hellojavaer.ddal.ddr.shard.ShardRouteContext.setDefaultRouteInfo},
+ * and this default route configuration will take effect only in the following case:
+ * 1. No 'sdKey' was specified in route rule configuration
+ * 2. Specified route rule in configuration but no 'sdKey' was found in sql and no associated route information was specified by #{org.hellojavaer.ddal.ddr.shard.ShardRouteContext.setRouteInfo}
  *
+ * 'ShardRoute' is designed base on 'one schema one route rule'. 'one schema one rule route' is a good practice.
+ * But if you need to access one more schemas which may have different route rule in one sql, you should add this annotation on its sub-invoking method.
+ *
+ *</pre>
  * @author <a href="mailto:hellojavaer@gmail.com">Kaiming Zou</a>,created on 01/01/2017.
  */
 @Target({ ElementType.METHOD })
