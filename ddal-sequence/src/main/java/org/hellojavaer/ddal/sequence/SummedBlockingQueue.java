580757098cdf562d98dc01a34ebea8eb6ff0776b
Kaiming Zou <hellojavaer@gmail.com>
2017-07-07 01:20:00 +0800

    update

--- ddal-sequence/src/main/java/org/hellojavaer/ddal/sequence/SummedBlockingQueue.java
+++ ddal-sequence/src/main/java/org/hellojavaer/ddal/sequence/SummedBlockingQueue.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.hellojavaer.ddal.sequence;
 
 import java.util.concurrent.TimeUnit;
@@ -9,7 +24,9 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 
- * this class is created based on java.util.concurrent.LinkedBlockingQueue, and is just for inner use.
+ * This class is similar to java.util.concurrent.LinkedBlockingQueue.
+ *
+ * And this class is just for inner use.
  * 
  * @author <a href="mailto:hellojavaer@gmail.com">Kaiming Zou</a>,created on 06/025/2017.
  */
