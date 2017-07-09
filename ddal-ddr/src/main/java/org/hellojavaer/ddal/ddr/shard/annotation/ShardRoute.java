9a634ad991894ccc2ecbe11e75623edbf631abfd
Kaiming Zou <hellojavaer@gmail.com>
2017-07-09 12:44:00 +0800

    update comment

--- ddal-ddr/src/main/java/org/hellojavaer/ddal/ddr/shard/annotation/ShardRoute.java
+++ ddal-ddr/src/main/java/org/hellojavaer/ddal/ddr/shard/annotation/ShardRoute.java
@@ -25,8 +25,9 @@ import java.lang.annotation.*;
  * 1. No 'sdKey' was specified in route rule configuration
  * 2. Specified route rule in configuration but no 'sdKey' was found in sql and no associated route information was specified by #{org.hellojavaer.ddal.ddr.shard.ShardRouteContext.setRouteInfo}
  *
- * 'ShardRoute' is designed base on 'one schema one route rule'. 'one schema one rule route' is a good practice.
- * But if you need to access one more schemas which may have different route rule in one sql, you should add this annotation on its sub-invoking method.
+ * 'ShardRoute' is designed base on 'one schema one route rule'.
+ *
+ * But if you need to access one more schemas which have different route rule in one sql, you should add this annotation on its sub-invoking method.
  *
  *</pre>
  * @author <a href="mailto:hellojavaer@gmail.com">Kaiming Zou</a>,created on 01/01/2017.
@@ -36,8 +37,17 @@ import java.lang.annotation.*;
 @Documented
 public @interface ShardRoute {
 
+    /**
+     * allow a comma-delimited list of scNames
+     * 
+     * @return
+     */
     String scName() default "";
 
+    /**
+     *
+     * @return
+     */
     String sdValue() default "";
 
 }
