81d413b57adb3499fd5b37c6eabfb239bfa21990
Kaiming Zou <hellojavaer@gmail.com>
2017-09-09 18:36:38 +0800

    implement HttpSequencRangeGetter

---
+++ ddal-sequence/src/main/java/org/hellojavaer/ddal/sequence/HttpSequencRangeGetter.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2017-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hellojavaer.ddal.sequence;
+
+import org.hellojavaer.ddal.sequence.exception.GetSequenceFailedException;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author <a href="mailto:hellojavaer@gmail.com">Kaiming Zou</a>,created on 09/09/2017.
+ */
+public class HttpSequencRangeGetter implements SequenceRangeGetter {
+
+    private String authorizeUrl;
+    private String clientId;
+    private String clientToken;
+    private String accessToken;
+    private String accessUrl;
+
+    public HttpSequencRangeGetter(String authorizeUrl, String clientId, String clientToken) {
+        this.authorizeUrl = authorizeUrl;
+        this.clientId = clientId;
+        this.clientToken = clientToken;
+    }
+
+    /**
+     * param: client_id=&client_token=
+     * return: access_url=http&access_token=1&error_code=1
+     */
+    private void authorize() {
+        Map<String, Object> param = new HashMap<>();
+        param.put("client_id", clientId);
+        param.put("client_token", clientToken);
+        String result = sendPost(this.authorizeUrl, param);
+        String[] kvs = result.split("&");
+        for (String item : kvs) {
+            String[] kv = item.split("=");
+            if ("error_code".equals(kv[0]) && kv.length >= 2 && kv[1] != null && kv[1].trim().length() > 0) {
+                throw new GetSequenceFailedException("client_id:" + clientId + " authorize failed, return message is: "
+                                                     + result);
+            } else if ("access_url".equals(kv[0])) {
+                this.accessUrl = kv[1];
+            } else if ("access_token".equals(kv[0])) {
+                this.accessToken = kv[1];
+            }
+        }
+        if (accessUrl != null) {
+            accessUrl = accessUrl.trim();
+        }
+        if (accessToken != null) {
+            accessToken = accessToken.trim();
+        }
+        if (accessUrl == null || accessUrl.length() == 0 || accessToken == null || accessToken.length() == 0) {
+            throw new GetSequenceFailedException("client_id:" + clientId + " authorize failed, return message is: "
+                                                 + result);
+        }
+    }
+
+    /**
+     * param: client_id=1&access_token=12
+     * return: begin_value=1&end_value=2
+     *
+     */
+    @Override
+    public SequenceRange get(String schemaName, String tableName, int step) throws Exception {
+        Map<String, Object> param = new HashMap<>();
+        param.put("client_id", clientId);
+        param.put("access_token", accessToken);
+        String result = sendPost(accessUrl, param);
+        String[] kvs = result.split("&");
+        Long beginValue = null;
+        Long endValue = null;
+        boolean hasErrorCode = false;
+        for (String item : kvs) {
+            String[] kv = item.split("=");
+            if ("error_code".equals(kv[0])) {
+                hasErrorCode = true;
+                break;
+            } else if ("begin_value".equals(kv[0])) {
+                beginValue = Long.valueOf(kv[1].trim());
+            } else if ("end_value".equals(kv[0])) {
+                endValue = Long.valueOf(kv[1].trim());
+            }
+        }
+        if (hasErrorCode) {
+            authorize();
+            result = sendPost(accessUrl, param);
+            kvs = result.split("&");
+            for (String item : kvs) {
+                String[] kv = item.split("=");
+                if ("error_code".equals(kv[0])) {
+                    throw new GetSequenceFailedException("client_id:" + clientId
+                                                         + " access data failed, return message is:" + result);
+                } else if ("begin_value".equals(kv[0])) {
+                    beginValue = Long.valueOf(kv[1].trim());
+                } else if ("end_value".equals(kv[0])) {
+                    endValue = Long.valueOf(kv[1].trim());
+                }
+            }
+        }
+        SequenceRange sequenceRange = new SequenceRange();
+        sequenceRange.setBeginValue(beginValue);
+        sequenceRange.setEndValue(endValue);
+        return sequenceRange;
+    }
+
+    public String sendGet(String url) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("clientId", clientId);
+        params.put("clientToken", clientToken);
+        return sendPost(url, params);
+    }
+
+    private String sendPost(String url, Map<String, Object> params) {
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+            con.setDoOutput(true);
+            // send
+            DataOutputStream wr = null;
+            try {
+                wr = new DataOutputStream(con.getOutputStream());
+                if (params != null && !params.isEmpty()) {
+                    StringBuilder sb = new StringBuilder();
+                    for (Map.Entry<String, Object> entry : params.entrySet()) {
+                        sb.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                        sb.append('=');
+                        sb.append(URLEncoder.encode(entry.getValue().toString(), "UTF-8"));
+                        sb.append('&');
+                    }
+                    sb.deleteCharAt(sb.length() - 1);
+                    wr.write(sb.toString().getBytes("UTF-8"));
+                }
+                wr.flush();
+            } finally {
+                closeIO(wr);
+            }
+            // get
+            BufferedReader in = null;
+            try {
+                int responseCode = con.getResponseCode();
+                if (responseCode != 200) {
+                    throw new IllegalStateException();
+                }
+                in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                return response.toString();
+            } finally {
+                closeIO(in);
+            }
+        } catch (Exception e) {
+            if (e instanceof RuntimeException) {
+                throw (RuntimeException) e;
+            } else {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void closeIO(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+}
