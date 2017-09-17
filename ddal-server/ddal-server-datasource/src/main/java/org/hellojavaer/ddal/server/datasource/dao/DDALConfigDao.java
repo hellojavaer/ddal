9fd2346cf1a827e464875f999a461276b6475949
Kaiming Zou <hellojavaer@gmail.com>
2017-09-18 00:06:15 +0800

    update

--- ddal-server/ddal-server-datasource/src/main/java/org/hellojavaer/ddal/server/datasource/dao/DDALConfigDao.java
+++
@@ -1,9 +0,0 @@
-package org.hellojavaer.ddal.server.datasource.dao;
-
-/**
- *
- * @author <a href="mailto:hellojavaer@gmail.com">Kaiming Zou</a>,created on 09/09/2017.
- */
-public class DDALConfigDao {
-
-}
--- ddal-server/ddal-server-datasource/src/main/java/org/hellojavaer/ddal/server/datasource/dao/impl/AppDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package org.hellojavaer.ddal.server.datasource.dao.impl;
-
-/**
- *
- * @author <a href="mailto:hellojavaer@gmail.com">Kaiming Zou</a>,created on 09/09/2017.
- */
-public class AppDaoImpl {
-
-}
