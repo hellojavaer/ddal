909fdb52b59a30365fd536c639fbf3675cc81621
Kaiming Zou <hellojavaer@gmail.com>
2017-09-28 00:22:43 +0800

    update DefaultDDALDataSource

--- ddal-datasource/src/main/java/org/hellojavaer/ddal/datasource/DefaultDDALDataSource.java
+++ ddal-datasource/src/main/java/org/hellojavaer/ddal/datasource/DefaultDDALDataSource.java
@@ -82,8 +82,7 @@ public class DefaultDDALDataSource implements DDALDataSource {
                 context = new ClassPathXmlApplicationContext(url2);
             } else if (url2.startsWith("file:")) {
                 context = new FileSystemXmlApplicationContext(url2);
-            } else if (url2.startsWith("//")) {
-                url2 = "http:" + url2;
+            } else if (url2.startsWith("http:") || url2.startsWith("https:")) {
                 Map<String, String> param = new LinkedHashMap<>();
                 param.put("username", username);
                 param.put("password", password);
